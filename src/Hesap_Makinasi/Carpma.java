3b170477d1c899163e99f3b555f33cb31b023691
Elif <elfgmlk@gmail.com>
2023-03-11 23:32:50 +0100

    deneme-hata var

--- src/Hesap_Makinasi/Carpma.java
+++ src/Hesap_Makinasi/Carpma.java
@@ -3,4 +3,26 @@ package Hesap_Makinasi;
 public class Carpma {
 
 
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        int sonuc =1;
+
+        System.out.println(carpma(5, 10));
+
+    }
+    public static int carpma (int a, int b) {
+
+        int sonuc = a*b;
+
+        return sonuc;
+    }
+
+
+    private static class String {
+    }
+
+    private static class System {
+    }
 }
