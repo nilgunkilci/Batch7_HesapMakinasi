d5e2445db3204973b2b483db4cf3bede9ec9e94e
Nilgün Kilci <awz89090@gmail.com>
2023-03-14 13:29:13 +0100

    Toplama class uploaded

--- src/Hesap_Makinasi/Test.java
+++ src/Hesap_Makinasi/Test.java
@@ -1,4 +1,8 @@
 package Hesap_Makinasi;
 
 public class Test {
+    public static void main(String[] args) {
+        Toplama top = new Toplama();    // Toplama classindan object olusturuyoruz
+        top.toplama(24,25);         // Toplama classinin metodunu cagiriyoruz.
+    }
 }
