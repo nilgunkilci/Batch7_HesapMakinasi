fd8bb4887b34b7e84ef42b239de2673600af49f0
Elif <elfgmlk@gmail.com>
2023-03-14 14:31:22 +0100

    Carpma islemi

--- src/Hesap_Makinasi/Carpma.java
+++ src/Hesap_Makinasi/Carpma.java
@@ -3,10 +3,7 @@ package Hesap_Makinasi;
 public class Carpma {
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int sonuc =1;
-        System.out.println(carpma(5, 10));
+
     }
     public static int carpma (int a, int b) {
         int sonuc = a*b;
--- src/Hesap_Makinasi/Test.java
+++ src/Hesap_Makinasi/Test.java
@@ -1,4 +1,9 @@
 package Hesap_Makinasi;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(Carpma.carpma(3, 5));
+    }
+
+
 }
