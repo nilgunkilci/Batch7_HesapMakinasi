5a493874d4a21fccf4a7c870c7c179d429c3dabb
Fatih <faydan80@outlook.com>
2023-03-15 14:49:10 +0100

    Hello GitHub2

--- src/Cikarma.java
+++ src/Cikarma.java
@@ -2,6 +2,7 @@ public class Cikarma {
     public static void main(String[] args) {
       int islemSonucu=cikarma(20,10);
         System.out.println(islemSonucu);
+
     }
     public static int cikarma(int a,int b){
        int sonuc = a-b;
